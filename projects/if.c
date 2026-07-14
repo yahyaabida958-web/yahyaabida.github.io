aff57faab3710cd2015fc5a395a7ea38a7b7e5d8
yahyaabida958-web <yahyaabida958@gmail.com>
2026-07-14 19:34:37 +0100

    Add comparison program for two integers

---
+++ projects/if.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+int x = get_int("x; ");
+int y = get_int("y; ");
+if(x < y)
+{
+printf("x is less than y\n");
+}
+else if(x > y)
+{
+ printf("x is not less than y\n");
+}
+else{
+    printf("x == y\n");
+}
+}
